19c882477471d49ad4183e429561de9146c90cca
vishwanath <101391216+viswa84@users.noreply.github.com>
2022-10-31 01:00:17 +0530

    laptop problem

--- my-default-starter/src/components/AniBox.js
+++ my-default-starter/src/components/AniBox.js
@@ -1,9 +1,35 @@
-  import React from 'react'
-  
-  const AniBox = () => {
-    return (
-      <div>AniBox</div>
-    )
-  }
-  
-  export default AniBox
+import React, { useState } from "react";
+import { useTransition, animated } from "react-spring";
+import "./AniBox.css";
+
+// React
+
+export default function AniBox() {
+  const [vis, Setvis] = useState(false);
+
+  const transform = useTransition(vis, {
+    from: { x: 800, y: -100, opacity: 0 },
+    enter: { x: 0, y: 0, opacity: 1, width: 200, height: 200 },
+    leave: { x: 800, y: 100, opacity: 0 }
+  });
+  const handleClick = () => {
+    Setvis(!vis);
+  };
+
+  return (
+    <div className="App">
+      <center>
+        <button onClick={handleClick}>{vis ? "unmount" : "mount"}</button>
+      </center>
+      <div className="container">
+        {transform((style, item) =>
+          item ? (
+            <animated.div style={style} className="item"></animated.div>
+          ) : (
+            ""
+          )
+        )}
+      </div>
+    </div>
+  );
+}
